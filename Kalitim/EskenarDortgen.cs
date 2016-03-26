56cad3b4c34b5e75912a5737ff595fd0c03617ee
Ismet Said Calik <ismetsaid.calik@gmail.com>
2016-03-26 21:41:32 +0200

    EskenarDortgen class olusturuldu.

---
+++ Kalitim/EskenarDortgen.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Kalitim
+{
+    public class EskenarDortgen
+    {
+        public EskenarDortgen(bool yon)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public EskenarDortgen(double yukseklik, double genislik)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public EskenarDortgen(bool yon, double yukseklik, double genislik)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public EskenarDortgen()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
