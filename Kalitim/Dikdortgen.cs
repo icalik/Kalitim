5e727c8664ebfb22422f4c9fa50fa6d5b267c417
Ismet Said Calik <ismetsaid.calik@gmail.com>
2016-03-26 21:37:50 +0200

    Dikdortgen class olusturuldu.

---
+++ Kalitim/Dikdortgen.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Kalitim
+{
+    public class Dikdortgen
+    {
+        private double Yukseklik;
+        private double Genislik;
+
+        public Dikdortgen(double yukseklik, double genislik)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Dikdortgen()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void setYukseklik(double yukseklik)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public double getYukseklik()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void setGenislik(double genislik)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public double getGenislik()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
