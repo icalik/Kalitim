9b393fe779acc1d834acfed71c8548e2f0541854
Ismet Said Calik <ismetsaid.calik@gmail.com>
2016-03-26 21:45:06 +0200

    ParalelKenar class olusturuldu.

---
+++ Kalitim/ParalelKenar.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Kalitim
+{
+    public class ParalelKenar
+    {
+        private bool Yon;
+
+        public ParalelKenar(bool yon)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public ParalelKenar(double yukseklik, double genislik)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public ParalelKenar(bool yon, double yukseklik, double genislik)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public ParalelKenar()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool getYon()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void setYon(bool yon)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
