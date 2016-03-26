04f0d266062b75dbe839f877f218cffab614425c
Ismet Said Calik <ismetsaid.calik@gmail.com>
2016-03-26 21:33:04 +0200

    Sekiller class olusturuldu.

---
+++ Kalitim/Sekiller.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Kalitim
+{
+    public abstract class Sekiller
+    {
+        private char Sembol;
+        private bool DoluMu;
+        private DateTime OlusturmaTarihi;
+
+        public Sekiller(char sembol)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Sekiller(char sembol, bool doluMu)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Sekiller()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void setSembol(char sembol)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public char getSembol()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void setDoluMu(bool doluMu)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool getDolumu()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void setOlusturmaTarihi(DateTime tarih)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public DateTime getOlusturmaTarihi()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public abstract double AlanHesapla();
+
+        public abstract double CevreHesapla();
+
+        public abstract void Ciz();
+
+        public abstract void OzellikYazdir();
+    }
+}
