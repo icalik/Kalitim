e31558ab56e1af8f8e8b2def671944e3c3ab5a71
Ismet Said Calik <ismetsaid.calik@gmail.com>
2016-03-26 21:39:24 +0200

    Ucgen class olusturuldu.

---
+++ Kalitim/Ucgen.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Kalitim
+{
+    public class Ucgen
+    {
+        private double Kenar;
+
+        public Ucgen(double kenar)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Ucgen()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void setKenar(double kenar)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public double getKenar()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
